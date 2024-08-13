c62f50135faebff7909e5ea46a5bd2bf20caeba1
net-bob <89314952+net-bob@users.noreply.github.com>
2024-08-13 09:36:12 -0700

    updates

--- Drone Code/optocoupler_v0.1/optocoupler_v0.1.ino
+++ Drone Code/optocoupler_v0.1/optocoupler_v0.1.ino
@@ -1,9 +1,12 @@
+// int optocouplerPin = 13;
+
 void setup() {
   // put your setup code here, to run once:
-
+  Serial.begin(9600);
+  pinMode(A0, INPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  Serial.println(analogRead(A0));
 }
